4cc67f8233599daaf750009bc017c81766b563fc
Joe McGrath <joseph.mcgrath@techworks.ie>
2021-04-20 16:43:20 +0100

    Added core functions, needs tidy

--- test.py
+++ test.py
@@ -1,82 +1,95 @@
 #!/usr/bin/python3
 import warnings
 import os
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 
 import matplotlib.pyplot as plt
 
-
-import lyricsgenius.song
 import pandas as pd
 import json
 
 import billboard
 
 from lyricsgenius import Genius
-from lyricsgenius import song
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import helpers
 
-def main():
 
-    #years = ['2016', '2017', '2018', '2019', '2020']
-    years = ['2019', '2020']
-    #years = ['2020']
-    genre = 'r-b-hip-hop-songs'
+def main():
+    y = ['2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
+        '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+    genre = 'hot-r-and-and-b-hip-hop-songs'
     g = 'QfcNFORWYYHMb2l48a95UsfzXqNTjnbJZkn3TZZ6HTquOw58d7JQdERD8VnOa71y'
     directory = '/home/joe/Desktop/lyrics/'
     h = Genius(g)
 
-    createFolders(years)
+    createFolders(y)
 
-    for i in range(len(years)):
+    for i in range(len(y)):
 
-        dir = directory + years[i]
-        r = billboard.ChartData('hot-r-and-and-b-hip-hop-songs', date=None, year=years[i], fetch=False)
+        dir = directory + y[i]
+        r = billboard.ChartData(genre, date=None, year=y[i], fetch=False)
 
         r.fetchEntries()
-        try:
-            for x in range(len(r)):
-                if 'Featuring' in r[x].artist:
-                    r[x].artist = r[x].artist.split('Featuring')[0]
-                if os.path.exists(dir + '/' + r[x].title + '.json'):
-                    continue
-                else:
-                    s = h.search_song(r[x].title, artist=r[x].artist)
-                    s.save_lyrics(filename=r[x].title, full_data=False, dic=dir, overwrite=True)
+        for x in range(len(r)):
+            if ',' in r[x].artist:
+                r[x].artist = r[x].artist.split(',')[0]
 
-        except Exception as e:
-            print(e)
-    df = readFiles(directory, years)
-    #year2019 = df[0]['words']
-    #year2019 = year2019.tolist()
-    print(len(df[0]['words']))
+            if '/' in r[x].title:
+                r[x].title = r[x].title.replace('/', '')
+
+            if '*' in r[x].title:   # Couldn't find how to remove profanity filter
+                continue
+
+            if '(' in r[x].title:
+                r[x].title = r[x].title.split('(')[0].rstrip()
+
+            if 'Featuring' in r[x].artist:
+                r[x].artist = r[x].artist.split('Featuring')
+
+            if os.path.exists(dir + '/' + r[x].title + '.json'):
+                continue
+
+            s = h.search_song(r[x].title, artist=r[x].artist[0])
+            l = 0
+
+            while s is None:
+                l =+ 1
+                s = h.search_song(r[x].title, artist=r[x].artist[l])
+                if l > len(r[x].artist):
+                    break
+
+            s.save_lyrics(filename=r[x].title, full_data=False, dic=dir, overwrite=True)
+
+    df = readFiles(directory, y)
+
+    years = []
     for i in range(len(df)):
         temp = []
         for x in range(len(df[i]['words'])):
-            temp.append(','.join(df[0]['words'][x]).lower())
+            temp.append(','.join(df[i]['words'][x]).lower())
         years.append(temp)
-
-    print(years[2])
-    print("------------------------------------------------------------------------")
-    print(years[3])
 
     plt.figure()
     wc = []
-    for x in range(len(years) - 2):
-        j = x + 2
-        wc.append(WordCloud(background_color="white", max_font_size=100, collocations=True))
-        wc[x].generate(str(years[j]))
-        plt.subplot(1, 4, j).set_title("Topic #" + str(x))
+    stop_words = ['im', 'got', 'yeah']
+
+    STOPWORDS.update(stop_words)
+
+    for x in range(len(y)):
+        j = x + 1
+        print(len(years[x]))
+
+        wc.append(WordCloud(background_color="white", max_font_size=300, collocations=True, stopwords=STOPWORDS))
+        wc[x].generate(str(years[x]))
+        plt.subplot(2, 5, j).set_title(y[x])
         plt.plot()
         plt.imshow(wc[x], interpolation='bilInear')
         plt.axis('off')
-    plt.suptitle("Year")
-    plt.show()
-        #temp = helpers.count(df)
 
-    #helpers.plot_wordcloud(temp, 2, 2)
+    plt.suptitle("Lyrics")
+    plt.show()
 
 def createFolders(years):
     for x in range(len(years)):
