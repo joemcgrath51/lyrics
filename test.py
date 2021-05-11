c938b7aed8020d890edde391fae19b056df21f58
joemcgrath51 <75248007+joemcgrath51@users.noreply.github.com>
2021-05-11 22:13:37 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -10,68 +10,70 @@ import json
 
 import billboard
 
-from lyricsgenius import Genius
+from lyricsgenius import Genius, song
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import helpers
 
 
 def main():
-    #y = ['2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011',
-    y = ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
-    genre = 'hot-rap-songs'
+
+    y = ['1991', '1992', '1993', '1994', '1995',
+         '1996', '1997', '1998', '1999', '2000', '2001',
+         '2002', '2003', '2004', '2005', '2006', '2007',
+         '2008', '2009', '2010', '2011', '2012', '2013',
+         '2014', '2015', '2016', '2017', '2018', '2019',
+         '2020']
+    """y = ['2001',
+         '2002', '2003', '2004', '2005', '2006', '2007',
+         '2008', '2009', '2010', '2011', '2012', '2013',
+         '2014', '2015', '2016', '2017', '2018', '2019',
+         '2020']"""
+
     g = 'QfcNFORWYYHMb2l48a95UsfzXqNTjnbJZkn3TZZ6HTquOw58d7JQdERD8VnOa71y'
-    directory = '/home/joe/Desktop/lyrics/'
+    #directory = '/home/joe/Desktop/lyrics/'
+    directory = 'D:\Downloads\\lyrics-main\\lyrics-main\\'
     h = Genius(g)
 
     createFolders(y)
-    test = 'r-b-hip-hop-songs'
-
-    #charts = helpers.get_charts(test, dates=helpers.get_dates_by_month(1990))
-    #n = helpers.get_n_most_frequent_entries(charts, 5)
-
-    #for x in range(len(n)):
-    #    temp = h.search_song(title=n[x])
-    #    print(temp)
-
-
-
+    genre = 'r-b-hip-hop-songs'
+    #"""
     for i in range(len(y)):
-        try:
-            dir = directory + y[i]
-            r = billboard.ChartData(genre, date=None, year=y[i], fetch=False)
 
-            r.fetchEntries()
-            for x in range(len(r)):
-                if ',' in r[x].artist:
-                    r[x].artist = r[x].artist.split(',')[0]
+        dir = directory + y[i]
 
-                if '/' in r[x].title:
-                    r[x].title = r[x].title.replace('/', '')
+        charts = helpers.get_charts(genre, dates=helpers.get_dates_by_month(int(y[i])))
+        n = helpers.get_n_most_frequent_entries(charts, 100)
 
-                if '*' in r[x].title:   # Couldn't find how to remove profanity filter
+        for x in range(len(n)):
+
+            name = n[x].split(",")[0]
+
+            if "'" in name:
+                name = name.replace("'", "’")
+
+            if not os.path.exists(dir + '/' + name + '.json'):
+                r = h.search_song(n[x])
+
+                if r is None:
                     continue
 
-                if '(' in r[x].title:
-                    r[x].title = r[x].title.split('(')[0].rstrip()
+                print(r.title)
+                print(name)
+                print(r.title.lower() not in name.lower())
 
-                if 'Featuring' in r[x].artist:
-                    r[x].artist = r[x].artist.split('Featuring')
-
-                if os.path.exists(dir + '/' + r[x].title + '.json'):
+                print(type(r))
+                if r.title.lower() not in name.lower():
                     continue
 
-                s = h.search_song(r[x].title, artist=r[x].artist[0])
-                l = 0
+                if "/" in name:
+                    name = name.replace("/", " ")
 
-                while s is None:
-                    l =+ 1
-                    s = h.search_song(r[x].title, artist=r[x].artist[l])
-                    if l > len(r[x].artist):
-                        break
-
-                s.save_lyrics(filename=r[x].title, full_data=False, dic=dir, overwrite=True)
-
+                #r.save_lyrics(filename=name, full_data=False, dic=dir,
+                              #overwrite=True)
+                r.save_lyrics(filename=name, dic=dir,
+                              overwrite=True)
+    #"""
     df = readFiles(directory, y)
 
     years = []
@@ -83,7 +85,7 @@ def main():
 
     plt.figure()
     wc = []
-    stop_words = ['im', 'got', 'yeah']
+    stop_words = ['im', 'got', 'yeah', 'na', 'oh', 'know', 'see']
 
     STOPWORDS.update(stop_words)
 
@@ -93,13 +95,15 @@ def main():
 
         wc.append(WordCloud(background_color="white", max_font_size=300, collocations=True, stopwords=STOPWORDS))
         wc[x].generate(str(years[x]))
-        plt.subplot(4, 5, j).set_title(y[x])
+        plt.subplot(int(str(len(y))[:1]) * 2, 5, j).set_title(y[x])
+
         plt.plot()
-        plt.imshow(wc[x], interpolation='bilInear')
+        plt.imshow(wc[x], interpolation='bilinear')
         plt.axis('off')
 
     plt.suptitle("Lyrics")
     plt.show()
+
 
 def createFolders(years):
     for x in range(len(years)):
@@ -108,7 +112,6 @@ def createFolders(years):
 
 
 def readFiles(dir, year):
-
     frames = []
 
     for i in range(len(year)):
@@ -140,5 +143,3 @@ def readFiles(dir, year):
 
 if __name__ == "__main__":
     main()
-
-
